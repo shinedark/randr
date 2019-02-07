a7a9f82e2273b974d24d7303ac8b14f7580388d6
shinedark <mide0303@gmail.com>
2019-02-07 15:41:51 -0600

    some music and cubes

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { View as GraphicsView } from 'expo-graphics';
 import ExpoTHREE, { THREE } from 'expo-three';
 import React from 'react';
-import { Asset} from 'expo';
+import {  Audio, Asset} from 'expo';
 
 
 function cacheImages(images) {
@@ -14,64 +14,105 @@ function cacheImages(images) {
   });
 }
 
+
 export default class App extends React.Component {
   componentWillMount() {
     THREE.suppressExpoWarnings();
-     this._loadAssetsAsync();
   }
 
-
-  async _loadAssetsAsync() {
-    const imageAssets = cacheImages([
-        require('./assets/videos/remastered.mp4')
-    ]);
-
-    await Promise.all([... imageAssets]);
+  async componentDidMount() {
+    Expo.Audio.setAudioModeAsync({
+      allowsRecordingIOS: false,
+      interruptionModeIOS: Expo.Audio.INTERRUPTION_MODE_IOS_DO_NOT_MIX,
+      playsInSilentModeIOS: true,
+      shouldDuckAndroid: true,
+      interruptionModeAndroid: Expo.Audio.INTERRUPTION_MODE_ANDROID_DO_NOT_MIX,
+    });
+    await this.playBackgroundMusicAsync();
+    await this._loadAssetsAsync();
   }
 
-
+  playBackgroundMusicAsync = async () => {
+    const soundObject = new Expo.Audio.Sound();
+    try {
+      await soundObject.loadAsync(require('./assets/audio/wuuu.wav'));
+      await soundObject.playAsync();
+      await soundObject.setStatusAsync({
+        shouldPlay: true,
+        isLooping: true,
+        volume: 1,
+      });
+      // Your sound is playing!
+    } catch (error) {
+      // An error occurred!
+    }
+  }
 
   render() {
     // Create an `ExpoGraphics.View` covering the whole screen, tell it to call our
     // `onContextCreate` function once it's initialized.
     return (
       <GraphicsView
-        style={{ flex: 1 }}
         onContextCreate={this.onContextCreate}
         onRender={this.onRender}
       />
     );
   }
 
+  async _loadAssetsAsync() {
+      const imageAssets = cacheImages([
+          
+          require('./assets/videos/giphy.gif'),
+          require('./assets/audio/wuuu.wav'),
+      ]);
+
+      await Promise.all([... imageAssets]);
+  }
   // This is called by the `ExpoGraphics.View` once it's initialized
   onContextCreate = async ({
     gl,
     canvas,
     width,
     height,
-    pixelRatio,
+    scale: pixelRatio,
   }) => {
     this.renderer = new ExpoTHREE.Renderer({ gl, pixelRatio, width, height });
     this.renderer.setClearColor(0xffffff)
     this.scene = new THREE.Scene();
     this.camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
     this.camera.position.z = 5;
+    
+    const texture = await ExpoTHREE.loadAsync(require('./assets/videos/giphy.gif'));
+      texture.needsupdate = true;
+      texture.minFilter = THREE.LinearFilter;
+      texture.magFilter = THREE.LinearFilter
+
     const geometry = new THREE.BoxGeometry(1, 1, 1);
 
-    // const texture = await ExpoTHREE.loadTextureAsync({ asset: });
-    const texture = await ExpoTHREE.loadAsync(require('./assets/videos/giphy.gif'));
-    texture.needsupdate = true;
-    texture.minFilter = THREE.LinearFilter;
-    texture.magFilter = THREE.LinearFilter;
-    // texture.format = THREE.RGBFormat;
-    const material = new THREE.MeshBasicMaterial({map:texture});
-      // material.transparent = false;
-      // material.side = THREE.DoubleSide;
-      // material.depthWrite = true;
-    
-    this.cube = new THREE.Mesh(geometry, material);
+    const material = new THREE.MeshPhongMaterial({
+      map: texture
+    });
 
+    const flakeGeometry = new THREE.BoxGeometry( 0.3, 0.3, 0.3 );
+    const flakeMaterial = new THREE.MeshPhongMaterial({map:texture});
+    const flakeCount = 9000;
+    const snow = new THREE.Group();
+    for (let i = 0; i < flakeCount; i++) {
+      const flakeMesh = new THREE.Mesh(flakeGeometry, flakeMaterial);
+      flakeMesh.position.set(
+        (Math.random() + 0.190) * 9,
+        (Math.random() - 0.160) * 10,
+        (Math.random() - 0.190) * 30
+      );
+      snow.add(flakeMesh);
+    }
+
+    // this.scene.add(snow);
+    
+    this.cube = snow;
+    flakeArray = this.cube.children;
     this.scene.add(this.cube);
+    
 
     this.scene.add(new THREE.AmbientLight(0x404040));
 
@@ -81,10 +122,28 @@ export default class App extends React.Component {
   };
 
   onRender = delta => {
-    this.cube.needsupdate = true;
-    this.cube.rotation.x += 3.5 * delta;
-    this.cube.rotation.y += 2 * delta;
+    for (i = 0; i < flakeArray.length / 2; i++) {
+       flakeArray[i].rotation.y += 0.01;
+       flakeArray[i].rotation.x += 0.02;
+       flakeArray[i].rotation.z += 0.03;
+       flakeArray[i].position.y -= 1;
+       if (flakeArray[i].position.y < -6) {
+         flakeArray[i].position.y += 100;
+       }
+     }
+     for (i = flakeArray.length / 2; i < flakeArray.length; i++) {
+       flakeArray[i].rotation.y -= 0.02;
+       flakeArray[i].rotation.x -= 0.03;
+       flakeArray[i].rotation.z -= 0.02;
+       flakeArray[i].position.y -= 0.1;
+       if (flakeArray[i].position.y < -6) {
+         flakeArray[i].position.y += 35;
+       }
+
+       this.cube.rotation.y -= 0.000009;
+     }
+    // this.cube.rotation.x += 1.5 * delta;
+    // this.cube.rotation.y += 1 * delta;
     this.renderer.render(this.scene, this.camera);
   };
 }
-
