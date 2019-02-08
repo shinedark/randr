6e6245ed0d8c59e7bde5faaf8d2a82059fe4f8b7
shinedark <mide0303@gmail.com>
2019-02-07 20:30:15 -0600

    cool stuff

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import { View as GraphicsView } from 'expo-graphics';
 import ExpoTHREE, { THREE } from 'expo-three';
 import React from 'react';
-import {  Audio, Asset} from 'expo';
+import {TouchableOpacity, View, Dimensions} from 'react-native';
+import {  Audio, Asset, Video} from 'expo';
 
 
 function cacheImages(images) {
@@ -14,8 +15,18 @@ function cacheImages(images) {
   });
 }
 
+let deviceWidth = Dimensions.get('window').width;
+let deviceHeight = Dimensions.get('window').height;
 
 export default class App extends React.Component {
+
+  constructor(props) {
+      super(props);
+      this.state = {
+        animations: false
+    };
+  }
+
   componentWillMount() {
     THREE.suppressExpoWarnings();
   }
@@ -28,23 +39,84 @@ export default class App extends React.Component {
       shouldDuckAndroid: true,
       interruptionModeAndroid: Expo.Audio.INTERRUPTION_MODE_ANDROID_DO_NOT_MIX,
     });
-    await this.playBackgroundMusicAsync();
+    
+    this.handlePLay1();
     await this._loadAssetsAsync();
   }
 
-  playBackgroundMusicAsync = async () => {
-    const soundObject = new Expo.Audio.Sound();
-    try {
-      await soundObject.loadAsync(require('./assets/audio/wuuu.wav'));
-      await soundObject.playAsync();
-      await soundObject.setStatusAsync({
-        shouldPlay: true,
-        isLooping: true,
-        volume: 1,
-      });
-      // Your sound is playing!
-    } catch (error) {
-      // An error occurred!
+
+  handlePLay1 = async () => {
+      const soundObject = new Expo.Audio.Sound();
+        try {
+          await soundObject.loadAsync(require('./assets/audio/intro.mp3'));
+          this.audioPlayer1  = soundObject;
+            this.audioPlayer1.setIsLoopingAsync(true);
+            this.audioPlayer1.setPositionAsync(0);
+            this.audioPlayer1.setVolumeAsync(0.8);
+            this.audioPlayer1.playAsync();
+            
+          
+          
+         // Your sound is playing!
+        } catch (error) {
+        // An error occurred!
+          
+        } 
+  }
+
+
+  stopMusic1 = async () => {
+    this.audioPlayer1.stopAsync();
+
+  }
+
+  handlePLay2 = async () => {
+      const soundObject = new Expo.Audio.Sound();
+        try {
+          await soundObject.loadAsync(require('./assets/audio/main.mp3'));
+          this.audioPlayer2  = soundObject;
+            this.audioPlayer2.setIsLoopingAsync(true);
+            this.audioPlayer2.setPositionAsync(0);
+            this.audioPlayer2.setVolumeAsync(0.8);
+            this.audioPlayer2.playAsync();
+            
+          
+          
+         // Your sound is playing!
+        } catch (error) {
+        // An error occurred!
+          
+        } 
+  }
+
+
+  stopMusic2 = async () => {
+    this.audioPlayer2.stopAsync();
+
+  }
+
+  pressBoom = async () =>{
+    if (!this.state.animations) {
+      
+      this.setState({animations: true});
+      this.handlePLay2();
+      this.stopMusic1();
+      
+      
+    } else {
+      this.setState({animations: false});
+      this.stopMusic2();
+      this.handlePLay1();
+      
+    }
+  }
+
+  animation =  () => {
+    if (!this.state.animations) {
+      this.onRender();
+    }
+    else{
+      this.onPress();
     }
   }
 
@@ -52,10 +124,12 @@ export default class App extends React.Component {
     // Create an `ExpoGraphics.View` covering the whole screen, tell it to call our
     // `onContextCreate` function once it's initialized.
     return (
+      <TouchableOpacity style={{flex: 1}} onPress={this.pressBoom}>
       <GraphicsView
         onContextCreate={this.onContextCreate}
-        onRender={this.onRender}
+        onRender={this.animation}
       />
+      </TouchableOpacity>
     );
   }
 
@@ -63,7 +137,8 @@ export default class App extends React.Component {
       const imageAssets = cacheImages([
           
           require('./assets/videos/giphy.gif'),
-          require('./assets/audio/wuuu.wav'),
+          require('./assets/audio/intro.mp3'),
+          require('./assets/audio/main.mp3'),
       ]);
 
       await Promise.all([... imageAssets]);
@@ -79,35 +154,29 @@ export default class App extends React.Component {
     this.renderer = new ExpoTHREE.Renderer({ gl, pixelRatio, width, height });
     this.renderer.setClearColor(0xffffff)
     this.scene = new THREE.Scene();
-    this.camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
-    this.camera.position.z = 5;
+    this.camera = new THREE.PerspectiveCamera(75, deviceWidth / deviceHeight, 0.1, 1000);
+    this.camera.position.z = 6;
     
     const texture = await ExpoTHREE.loadAsync(require('./assets/videos/giphy.gif'));
       texture.needsupdate = true;
       texture.minFilter = THREE.LinearFilter;
       texture.magFilter = THREE.LinearFilter
+      // texture.format = THREE.RGBFormat;
 
-    const geometry = new THREE.BoxGeometry(1, 1, 1);
-
-    const material = new THREE.MeshPhongMaterial({
-      map: texture
-    });
-
-    const flakeGeometry = new THREE.BoxGeometry( 0.3, 0.3, 0.3 );
+    const flakeGeometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
     const flakeMaterial = new THREE.MeshPhongMaterial({map:texture});
+    flakeMaterial.map.needsUpdate = true;
     const flakeCount = 9000;
     const snow = new THREE.Group();
     for (let i = 0; i < flakeCount; i++) {
       const flakeMesh = new THREE.Mesh(flakeGeometry, flakeMaterial);
       flakeMesh.position.set(
-        (Math.random() + 0.190) * 9,
-        (Math.random() - 0.160) * 10,
-        (Math.random() - 0.190) * 30
+        (Math.random() - 0.190) * 90,
+        (Math.random() - 0.16) * 10,
+        (Math.random() + 0.990) * 3
       );
       snow.add(flakeMesh);
     }
-
-    // this.scene.add(snow);
     
     this.cube = snow;
     flakeArray = this.cube.children;
@@ -121,7 +190,8 @@ export default class App extends React.Component {
     this.scene.add(light);
   };
 
-  onRender = delta => {
+
+  onPress = delta => {
     for (i = 0; i < flakeArray.length / 2; i++) {
        flakeArray[i].rotation.y += 0.01;
        flakeArray[i].rotation.x += 0.02;
@@ -140,10 +210,16 @@ export default class App extends React.Component {
          flakeArray[i].position.y += 35;
        }
 
-       this.cube.rotation.y -= 0.000009;
+       this.cube.rotation.y += 0.000009;
+       
      }
-    // this.cube.rotation.x += 1.5 * delta;
-    // this.cube.rotation.y += 1 * delta;
+     this.renderer.render(this.scene, this.camera);
+  };
+
+  onRender = delta => {
+
+    this.cube.rotation.x += 0.00009;
+    this.cube.rotation.y -= 0.00003;
     this.renderer.render(this.scene, this.camera);
   };
 }
